926e272ab329f082d065875c6402e65361bb51e8
BarbaraKuofie <barbara.kuofie@gmail.com>
2020-10-05 06:08:00 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,15 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, World You Win You Lose"
     resp.finish
   end
 
+  def greeting(env)
+      return [ 200, {'Content-Type' => 'text/html'}, last_response ]
+  
+    def last_response
+      (Time.now.to_i <= 12).zero? ?  ["Good Morning!"] : ["Good Afternoon!"]
+    end
+  end
 end
